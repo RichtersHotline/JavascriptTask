d9787e02c19942d6a999afbcb5ae38f830a49a66
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-05-31 13:52:06 +0200

    task

---
+++ Scripts/code.js
@@ -0,0 +1,28 @@
+
+// Way 1 Swapping using math and assignment
+// let first_number = 15;
+// let second_number = 17;
+// console.log(first_number, second_number)
+// first_number = second_number 
+// second_number = first_number - 2
+// console.log(first_number)
+// console.log(second_number)
+
+//Way 2 Swapping with an If statement
+// let first_number = 15;
+// let second_number = 17;
+
+// if (first_number == 15) {
+
+//     first_number = 17;
+// second_number = 15
+
+// }
+// console.log(first_number, second_number)
+
+//Way 3 Swapping with an Array
+
+// let first_numberAndsecond_Number = [17, 15]
+
+// first_numberAndsecond_Number[0] = first_numberAndsecond_Number.splice(1,1, first_numberAndsecond_Number[0]) [0]
+// console.log(first_numberAndsecond_Number)
